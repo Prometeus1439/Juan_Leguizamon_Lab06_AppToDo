c594a5eeea24e656b121c29b575e3726b628ab19
unknown <juan.leguizamon-r@labinfo.is.escuelaing.edu.co>
2026-09-21 09:59:37 -0500

    feat: Maven configuration with Spring Initializr

---
+++ todo-fullstack/backend/src/main/java/edu/eci/dosw/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiApplication.class, args);
+	}
+
+}
